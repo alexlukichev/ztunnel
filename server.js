e387ef0a05ebc7c4bf8060eef1ab9d2f6fd947e8
Alexander Lukichev <alexander.s.lukichev@gmail.com>
2015-05-11 23:13:56 -0500

    c

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello WorldYYY!');
+  res.send('Hello WorldZZZ!');
 });
 
 var server = app.listen(5000, function () {
